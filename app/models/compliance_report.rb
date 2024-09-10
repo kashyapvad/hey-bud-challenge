b4e0118c6940644d8a634bf7c1bc624f9dbaea3b
kashyapvad <kashyap.vad@gmail.com>
2024-09-09 18:32:35 -0700

    git ignore logs

--- app/models/compliance_report.rb
+++ app/models/compliance_report.rb
@@ -2,7 +2,19 @@ class ComplianceReport
   include Mongoid::Document
   include Mongoid::Timestamps
 
+
+  REQUIRED_FILES = {
+    extraction_guide: nil,
+    compliance_rules: {
+    },
+    report_templates: {
+
+    },
+  }
+
   field :plan, type: :string
+  field :files, type: :hash, default: REQUIRED_FILES
+  field :prompt, type: :string
   field :governance_body, type: :string
   field :complete, type: :boolean, default: false
 
